37f4e4afb0ef5038f2c00746730e78a9c9cdcfb7
pankajgupta74 <pankaj_gupta74@yahoo.com>
2016-05-16 21:37:52 +0530

    pom change

--- src/main/java/com/rbs/order/controller/OrderController.java
+++ src/main/java/com/rbs/order/controller/OrderController.java
@@ -22,6 +22,7 @@ public class OrderController {
 	@Autowired
 	private OrderService service;	
 
+	
 	@CrossOrigin(origins = {"http://webapp-dummycompany.rhcloud.com", "http://localhost:8080"})
 	@RequestMapping("/order")
 	public @ResponseBody Receipt order(@RequestBody List<PurchaseItem> items) {
