3029d72b6d8cd067ce63c820bb10bff0f29c1c76
pankajgupta74 <pankaj_gupta74@yahoo.com>
2016-05-18 07:33:06 +0530

    Update OrderController.java

--- src/main/java/com/rbs/order/controller/OrderController.java
+++ src/main/java/com/rbs/order/controller/OrderController.java
@@ -22,7 +22,7 @@ public class OrderController {
 	@Autowired
 	private OrderService service;	
 
-	@CrossOrigin(origins = "*")
+	@CrossOrigin(origins = "*"
 	@RequestMapping("/order")
 	public @ResponseBody Receipt order(@RequestBody List<PurchaseItem> items) {
 		
